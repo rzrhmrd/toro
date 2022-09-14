4a566756edecd4b714956a9344a227f45a6af50b
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 11:54:04 +0430

    implement insert note use case

--- app/src/main/java/com/rzmmzdh/toro/feature_note/domain/usecase/InsertNote.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/domain/usecase/InsertNote.kt
@@ -1,7 +1,11 @@
 package com.rzmmzdh.toro.feature_note.domain.usecase
 
 import com.rzmmzdh.toro.feature_note.data.repository.NoteRepository
+import com.rzmmzdh.toro.feature_note.domain.model.Note
+import javax.inject.Inject
 
-class InsertNote(noteRepository: NoteRepository){
-
+class InsertNote @Inject constructor(private val noteRepository: NoteRepository) {
+    suspend operator fun invoke(note: Note) {
+        noteRepository.insertNote(note)
+    }
 }
