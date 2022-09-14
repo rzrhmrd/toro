9449b34562e1ce30775ae9260e9d30768ab6f2a9
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 13:50:49 +0430

    add type converters

--- app/src/main/java/com/rzmmzdh/toro/feature_note/data/datasource/local/NoteDatabase.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/data/datasource/local/NoteDatabase.kt
@@ -2,8 +2,10 @@ package com.rzmmzdh.toro.feature_note.data.datasource.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [NoteEntity::class], version = 1)
+@TypeConverters(value = [DateConverter::class])
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
