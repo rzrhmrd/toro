e7745bdbc4724edef8abef2bac30a1c2938b78db
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 13:50:26 +0430

    add to note entity mapper

--- app/src/main/java/com/rzmmzdh/toro/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/domain/model/Note.kt
@@ -1,5 +1,9 @@
 package com.rzmmzdh.toro.feature_note.domain.model
 
+import com.rzmmzdh.toro.feature_note.data.datasource.local.NoteEntity
 import java.util.*
 
 data class Note(val id: Int, val title: String, val body: String, val lastModificationDate: Date)
+
+fun Note.toNoteEntity() =
+    NoteEntity(id = id, title = title, body = body, lastModificationDate = lastModificationDate)
