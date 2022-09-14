080c012e46c43a2875a6cea2610f9ce8ea4ab64a
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 12:40:36 +0430

    add note module

---
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/di/NoteModule.kt
@@ -0,0 +1,10 @@
+package com.rzmmzdh.toro.feature_note.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NoteModule {
+}
