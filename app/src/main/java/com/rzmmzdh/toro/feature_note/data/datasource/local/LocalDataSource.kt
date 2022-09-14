e5c2b3da116e2771a3c5faa4ba322ae7fce9be09
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 14:02:04 +0430

    add note mapping

--- app/src/main/java/com/rzmmzdh/toro/feature_note/data/datasource/local/LocalDataSource.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/data/datasource/local/LocalDataSource.kt
@@ -2,23 +2,26 @@ package com.rzmmzdh.toro.feature_note.data.datasource.local
 
 import com.rzmmzdh.toro.feature_note.data.datasource.NoteDataSource
 import com.rzmmzdh.toro.feature_note.domain.model.Note
+import com.rzmmzdh.toro.feature_note.domain.model.toNoteEntity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import javax.inject.Inject
 
 class LocalDataSource @Inject constructor(private val noteDao: NoteDao) : NoteDataSource {
     override suspend fun insertNote(note: Note) {
-        noteDao.insertNote(note)
+        noteDao.insertNote(note.toNoteEntity())
     }
 
     override fun getNote(id: Int): Flow<Note> {
-        return noteDao.getNote(id)
+        return noteDao.getNote(id).map { it.toNote() }
     }
 
     override fun getAllNotes(): Flow<List<Note>> {
-        return noteDao.getAllNotes()
+        return noteDao.getAllNotes().mapNotNull { it.map { it.toNote() } }
     }
 
     override suspend fun deleteNote(note: Note) {
-        noteDao.deleteNote(note)
+        noteDao.deleteNote(note.toNoteEntity())
     }
 }
