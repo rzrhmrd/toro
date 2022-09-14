67b980fefa976a3eaae3ef1117be80f1ad48fa39
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 11:54:46 +0430

    implement note repository

---
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package com.rzmmzdh.toro.feature_note.data.repository
+
+import com.rzmmzdh.toro.feature_note.domain.model.Note
+
+class NoteRepositoryImpl : NoteRepository {
+    override suspend fun insertNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+}
