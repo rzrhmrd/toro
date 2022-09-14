8c6bbdaa0cdc7a70267a1dc940cf1db3c12ed764
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 11:58:35 +0430

    implement insert note

--- app/src/main/java/com/rzmmzdh/toro/feature_note/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.rzmmzdh.toro.feature_note.data.repository
 
+import com.rzmmzdh.toro.feature_note.data.datasource.NoteDataSource
 import com.rzmmzdh.toro.feature_note.domain.model.Note
+import javax.inject.Inject
 
-class NoteRepositoryImpl : NoteRepository {
+class NoteRepositoryImpl @Inject constructor(private val localDataSource: NoteDataSource) :
+    NoteRepository {
     override suspend fun insertNote(note: Note) {
-        TODO("Not yet implemented")
+        localDataSource.insertNote(note)
     }
 }
