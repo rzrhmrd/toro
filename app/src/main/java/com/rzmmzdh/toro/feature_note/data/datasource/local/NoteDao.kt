d6846d457e5f938892e48c6d8a8ef0b02913229a
rzmmzdh <rzmmzdh@gmail.com>
2022-09-14 13:34:55 +0430

    update dao

--- app/src/main/java/com/rzmmzdh/toro/feature_note/data/datasource/local/NoteDao.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/data/datasource/local/NoteDao.kt
@@ -1,13 +1,20 @@
 package com.rzmmzdh.toro.feature_note.data.datasource.local
 
-import androidx.room.Dao
+import androidx.room.*
 import com.rzmmzdh.toro.feature_note.domain.model.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: Note)
+
+    @Query("SELECT * FROM noteentity WHERE id = :id")
     fun getNote(id: Int): Flow<Note>
+
+    @Query("SELECT * FROM noteentity")
     fun getAllNotes(): Flow<List<Note>>
+
+    @Delete
     fun deleteNote(note: Note)
 }
