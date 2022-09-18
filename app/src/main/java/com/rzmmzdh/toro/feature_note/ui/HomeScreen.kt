50ccdebe04b042d6e4468c63af972fbf9816db30
rzmmzdh <rzmmzdh@gmail.com>
2022-09-18 11:38:18 +0430

    print hello world

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
@@ -1,4 +1,19 @@
 package com.rzmmzdh.toro.feature_note.ui
 
-class HomeScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier) { paddingValues ->
+        Column(modifier = Modifier.padding(top = paddingValues.calculateTopPadding())) {
+            Text("Hello World")
+        }
+    }
 }
