dd9930806c86ad01f757163556c569cd53d5666e
rzmmzdh <rzmmzdh@gmail.com>
2022-09-18 16:14:04 +0430

    add note use cases container

---
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/domain/usecase/NoteUseCases.kt
@@ -0,0 +1,9 @@
+package com.rzmmzdh.toro.feature_note.domain.usecase
+
+data class NoteUseCases(
+    val getNote: GetNote,
+    val getAllNotes: GetAllNotes,
+    val insertNote: InsertNote,
+    val deleteNote: DeleteNote,
+    val deleteAllNotes: DeleteAllNotes
+)
