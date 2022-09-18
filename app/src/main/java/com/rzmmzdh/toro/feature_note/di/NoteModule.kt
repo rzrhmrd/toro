38fffc9bcc3f1a3a98d496257e69bedd920068fe
rzmmzdh <rzmmzdh@gmail.com>
2022-09-18 16:14:53 +0430

    provide note use cases

--- app/src/main/java/com/rzmmzdh/toro/feature_note/di/NoteModule.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/di/NoteModule.kt
@@ -2,12 +2,15 @@ package com.rzmmzdh.toro.feature_note.di
 
 import android.app.Application
 import androidx.room.Room
+import com.rzmmzdh.toro.feature_note.data.datasource.local.LocalDataSource
 import com.rzmmzdh.toro.feature_note.data.datasource.local.NoteDao
 import com.rzmmzdh.toro.feature_note.data.datasource.local.NoteDatabase
+import com.rzmmzdh.toro.feature_note.data.repository.NoteRepositoryImpl
+import com.rzmmzdh.toro.feature_note.domain.repository.NoteRepository
+import com.rzmmzdh.toro.feature_note.domain.usecase.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -22,11 +25,35 @@ class NoteModule {
 
     @Provides
     @Singleton
-    fun provideNoteDatabase(@ApplicationContext context: Application): NoteDatabase {
+    fun provideNoteDatabase(context: Application): NoteDatabase {
         return Room.databaseBuilder(
             context,
             NoteDatabase::class.java,
             "note_database.db"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalDataSource(noteDao: NoteDao): LocalDataSource {
+        return LocalDataSource(noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(localDataSource: LocalDataSource): NoteRepository {
+        return NoteRepositoryImpl(localDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNote = GetNote(repository),
+            getAllNotes = GetAllNotes(repository),
+            insertNote = InsertNote(repository),
+            deleteNote = DeleteNote(repository),
+            deleteAllNotes = DeleteAllNotes(repository)
+        )
     }
 }
