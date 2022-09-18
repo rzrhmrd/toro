3632303e44fa035873e41d3be4efae337f6b71f4
rzmmzdh <rzmmzdh@gmail.com>
2022-09-18 11:37:59 +0430

    implement nav host

--- app/src/main/java/com/rzmmzdh/toro/MainActivity.kt
+++ app/src/main/java/com/rzmmzdh/toro/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rzmmzdh.toro.feature_note.ui.Destination
+import com.rzmmzdh.toro.feature_note.ui.HomeScreen
 import com.rzmmzdh.toro.theme.TOROTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +26,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Destination.HomeScreen.route
+                    ) {
+                        composable(Destination.HomeScreen.route) {
+                            HomeScreen()
+                        }
+                    }
                 }
             }
         }
