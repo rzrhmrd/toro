200ddccc40a0e0af9c1601e1ad441126d01d08ca
rzmmzdh <rzmmzdh@gmail.com>
2022-09-19 14:02:35 +0430

    implement note card

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
@@ -3,7 +3,7 @@ package com.rzmmzdh.toro.feature_note.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Delete
@@ -11,7 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.rzmmzdh.toro.feature_note.viewmodel.HomeScreenViewModel
 
@@ -42,6 +42,7 @@ fun MainTopBar(viewModel: HomeScreenViewModel) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteList(
     paddingValues: PaddingValues,
@@ -56,10 +57,33 @@ fun NoteList(
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             modifier = Modifier
-                .padding(top = paddingValues.calculateTopPadding()),
+                .padding(
+                    top = paddingValues.calculateTopPadding(),
+                    start = 8.dp,
+                    end = 8.dp
+                ),
         ) {
-            items(items = viewModel.noteList.value.notes) {
-                Text(it.title, textAlign = TextAlign.Center)
+            itemsIndexed(items = viewModel.noteList.value.notes) { index, note ->
+                Card(
+                    modifier = Modifier
+                        .size(192.dp)
+                        .padding(all = 8.dp),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            viewModel.noteList.value.notes[index].title,
+                        )
+                        Text(
+                            viewModel.noteList.value.notes[index].body,
+                        )
+                    }
+
+                }
             }
         }
 
