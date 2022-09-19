f22b3ac7621f7e039e86ecc2cb50f6919534871e
rzmmzdh <rzmmzdh@gmail.com>
2022-09-19 12:41:42 +0430

    display a simple list of notes

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
@@ -1,19 +1,74 @@
 package com.rzmmzdh.toro.feature_note.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.rzmmzdh.toro.feature_note.viewmodel.HomeScreenViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    Scaffold(modifier = modifier) { paddingValues ->
-        Column(modifier = Modifier.padding(top = paddingValues.calculateTopPadding())) {
-            Text("Hello World")
+fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeScreenViewModel = hiltViewModel()) {
+    Scaffold(
+        modifier = modifier,
+        floatingActionButton = {
+            AddNoteFab(viewModel)
+        },
+        topBar = { MainTopBar(viewModel) }
+    ) { paddingValues ->
+        NoteList(paddingValues, viewModel)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(viewModel: HomeScreenViewModel) {
+    CenterAlignedTopAppBar(
+        title = { Text("toro") },
+        actions = {
+            IconButton(onClick = { viewModel.onEvent(HomeScreenEvent.DeleteAllNotes) }) {
+                Icon(Icons.Rounded.Delete, "delete all notes")
+            }
         }
+    )
+}
+
+@Composable
+fun NoteList(
+    paddingValues: PaddingValues,
+    viewModel: HomeScreenViewModel,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .padding(top = paddingValues.calculateTopPadding()),
+        ) {
+            items(items = viewModel.noteList.value.notes) {
+                Text(it.title, textAlign = TextAlign.Center)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun AddNoteFab(viewModel: HomeScreenViewModel) {
+    FloatingActionButton(onClick = { viewModel.onEvent(HomeScreenEvent.AddNewNote) }) {
+        Icon(Icons.Rounded.Add, contentDescription = "Add a new note")
     }
 }
---
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/viewmodel/HomeScreenViewModel.kt
@@ -0,0 +1,51 @@
+package com.rzmmzdh.toro.feature_note.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rzmmzdh.toro.feature_note.domain.model.Note
+import com.rzmmzdh.toro.feature_note.domain.usecase.NoteUseCases
+import com.rzmmzdh.toro.feature_note.ui.HomeScreenEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeScreenViewModel @Inject constructor(
+    private val noteUseCases: NoteUseCases,
+) : ViewModel() {
+    init {
+        viewModelScope.launch {
+            repeat(12) {
+                val sampleNote = Note(Random().nextInt(48), "Title", "Body", Date())
+                noteUseCases.insertNote(sampleNote)
+            }
+            noteUseCases.getAllNotes().collectLatest {
+                noteList.value = noteList.value.copy(notes = it)
+            }
+        }
+    }
+
+
+    var noteList = mutableStateOf(NoteListUiState())
+        private set
+
+    fun onEvent(event: HomeScreenEvent) {
+        when (event) {
+            HomeScreenEvent.AddNewNote -> {
+                val sampleNewNote = Note(Random().nextInt(), "a new title", "a new body", Date())
+                viewModelScope.launch {
+                    noteUseCases.insertNote(sampleNewNote)
+                }
+            }
+            HomeScreenEvent.DeleteAllNotes -> {
+                viewModelScope.launch {
+                    noteUseCases.deleteAllNotes()
+                }
+
+            }
+        }
+    }
+}
