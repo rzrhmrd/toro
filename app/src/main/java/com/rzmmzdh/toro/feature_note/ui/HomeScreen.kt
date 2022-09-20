ffc3c33b09732f2f44d151bb9d77bad68c3f7677
rzmmzdh <rzmmzdh@gmail.com>
2022-09-20 17:45:11 +0430

    implement home screen

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/HomeScreen.kt
@@ -5,79 +5,51 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.rzmmzdh.toro.feature_note.viewmodel.HomeScreenViewModel
+import androidx.navigation.NavController
+import com.rzmmzdh.toro.R
+import com.rzmmzdh.toro.feature_note.ui.core.Screens
+import com.rzmmzdh.toro.feature_note.ui.viewmodel.HomeScreenEvent
+import com.rzmmzdh.toro.feature_note.ui.viewmodel.HomeScreenViewModel
 import com.rzmmzdh.toro.theme.vazirFontFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeScreenViewModel = hiltViewModel()) {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeScreenViewModel = hiltViewModel(),
+    navController: NavController
+) {
     Scaffold(
         modifier = modifier,
-        floatingActionButton = {
-            AddNoteFab(viewModel)
-        },
-        topBar = { MainTopBar(viewModel) },
+        topBar = { MainTopBar(stringResource(R.string.toro)) },
         bottomBar = {
-            MainNavigationBar()
+            MainNavigationBar(navController, currentScreen = Screens.Home)
         }
     ) { paddingValues ->
         NoteList(paddingValues, viewModel)
     }
 }
 
-@Composable
-fun MainNavigationBar() {
-    val selectedDestination by remember {
-        mutableStateOf(Destination.HomeScreen)
-    }
-    val items = listOf(Destination.HomeScreen, Destination.Settings)
-    NavigationBar(modifier = Modifier) {
-        items.forEachIndexed { index, item ->
-            NavigationBarItem(
-                selected = selectedDestination == item,
-                onClick = { /*TODO*/ },
-                icon = {
-                    when (item) {
-                        Destination.Settings -> Icon(
-                            Icons.Rounded.Settings,
-                            "settings screen navigation item"
-                        )
-                        Destination.HomeScreen -> Icon(
-                            Icons.Rounded.Home,
-                            "home screen navigation item"
-                        )
-
-
-                    }
-                },
-            )
-        }
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainTopBar(viewModel: HomeScreenViewModel) {
+fun MainTopBar(title: String) {
     CenterAlignedTopAppBar(
         title = {
             Text(
-                "تورو",
+                title,
                 style = TextStyle(
                     fontFamily = vazirFontFamily,
                     fontWeight = FontWeight.Black,
@@ -85,11 +57,6 @@ fun MainTopBar(viewModel: HomeScreenViewModel) {
                 )
             )
         },
-        actions = {
-            IconButton(onClick = { viewModel.onEvent(HomeScreenEvent.DeleteAllNotes) }) {
-                Icon(Icons.Rounded.Delete, "delete all notes")
-            }
-        }
     )
 }
 
@@ -98,7 +65,6 @@ fun MainTopBar(viewModel: HomeScreenViewModel) {
 fun NoteList(
     paddingValues: PaddingValues,
     viewModel: HomeScreenViewModel,
-    modifier: Modifier = Modifier
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
@@ -107,16 +73,15 @@ fun NoteList(
             .padding(
                 top = paddingValues.calculateTopPadding(),
                 bottom = paddingValues.calculateBottomPadding(),
-                start = 8.dp,
-                end = 8.dp
-            ),
+            )
+            .padding(horizontal = 8.dp),
     ) {
-        itemsIndexed(items = viewModel.noteList.value.notes) { index, note ->
+        itemsIndexed(items = viewModel.notes.value.notes) { index, note ->
             Card(
                 modifier = Modifier
                     .size(192.dp)
                     .padding(all = 8.dp),
-                onClick = { /*TODO*/ }
+                onClick = { viewModel.onEvent(HomeScreenEvent.DeleteNote(note)) }
             ) {
                 Column(
                     modifier = Modifier.fillMaxSize(),
@@ -124,7 +89,7 @@ fun NoteList(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        viewModel.noteList.value.notes[index].title,
+                        viewModel.notes.value.notes[index].title,
                         style = TextStyle(
                             fontFamily = vazirFontFamily,
                             fontSize = 16.sp,
@@ -132,7 +97,7 @@ fun NoteList(
                         )
                     )
                     Text(
-                        viewModel.noteList.value.notes[index].body,
+                        viewModel.notes.value.notes[index].body,
                         style =
                         TextStyle(
                             fontFamily = vazirFontFamily,
@@ -149,19 +114,34 @@ fun NoteList(
 }
 
 @Composable
-fun AddNoteFab(viewModel: HomeScreenViewModel) {
-    ExtendedFloatingActionButton(
-        onClick = { viewModel.onEvent(HomeScreenEvent.AddNewNote) },
-        icon = { Icon(Icons.Rounded.Add, "add note fab") },
-        text = {
-            Text(
-                "یادداشت",
-                style = TextStyle(
-                    fontFamily = vazirFontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp
+fun MainNavigationBar(navController: NavController, currentScreen: Screens) {
+    var selectedDestination by remember { mutableStateOf(currentScreen) }
+    val items = listOf(Screens.Settings, Screens.Home, Screens.EditNote)
+    NavigationBar(modifier = Modifier) {
+        items.forEach { item ->
+            when (item) {
+                Screens.Settings -> NavigationBarItem(
+                    selected = selectedDestination == item,
+                    onClick = { navController.navigate(Screens.Settings.route) },
+                    icon = {
+                        Icon(
+                            Icons.Rounded.Settings,
+                            stringResource(R.string.settings_icon)
+                        )
+                    }
                 )
-            )
+                Screens.Home -> NavigationBarItem(
+                    selected = selectedDestination == item,
+                    onClick = { navController.navigate(Screens.Home.route) },
+                    icon = { Icon(Icons.Rounded.Home, stringResource(R.string.home_screen_icon)) }
+                )
+                Screens.EditNote -> NavigationBarItem(
+                    selected = selectedDestination == item,
+                    onClick = { navController.navigate(Screens.EditNote.route) },
+                    icon = { Icon(Icons.Rounded.Edit, stringResource(R.string.edit_note_icon)) }
+                )
+
+            }
         }
-    )
+    }
 }
