7868063c4a01dcdc61dcf6c01000232787a433d0
rzmmzdh <rzmmzdh@gmail.com>
2022-09-20 17:42:07 +0430

    rename destinations to screens

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/Destination.kt
+++
@@ -1,6 +0,0 @@
-package com.rzmmzdh.toro.feature_note.ui
-
-sealed class Destination(val route: String) {
-    object HomeScreen : Destination("/home_screen")
-    object Settings : Destination("/settings")
-}
