c7198d5bd44a9e00c9df918222bdc05dbe506c9b
rzmmzdh <rzmmzdh@gmail.com>
2022-09-23 18:15:26 +0330

    add open dialog event

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/viewmodel/EditNoteEvent.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/viewmodel/EditNoteEvent.kt
@@ -1,9 +1,8 @@
 package com.rzmmzdh.toro.feature_note.ui.viewmodel
 
-import com.rzmmzdh.toro.feature_note.domain.model.Note
-
 sealed class EditNoteEvent {
-    data class SaveNote(val note: Note) : EditNoteEvent()
+    object SaveNote : EditNoteEvent()
+    object OpenDialog : EditNoteEvent()
     data class OnTitleChanged(val value: String) : EditNoteEvent()
     data class OnBodyChanged(val value: String) : EditNoteEvent()
 }
