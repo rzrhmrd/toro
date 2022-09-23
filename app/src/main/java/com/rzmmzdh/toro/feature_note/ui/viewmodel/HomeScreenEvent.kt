2a20f94a7128fd628d6a4a966b65478b2ddde418
rzmmzdh <rzmmzdh@gmail.com>
2022-09-23 18:21:33 +0330

    remove redundant events

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/viewmodel/HomeScreenEvent.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/viewmodel/HomeScreenEvent.kt
@@ -4,8 +4,5 @@ import com.rzmmzdh.toro.feature_note.domain.model.Note
 
 
 sealed class HomeScreenEvent {
-    object AddNewNote : HomeScreenEvent()
-    object DeleteAllNotes : HomeScreenEvent()
-    object ToggleTheme : HomeScreenEvent()
     data class DeleteNote(val note: Note) : HomeScreenEvent()
 }
