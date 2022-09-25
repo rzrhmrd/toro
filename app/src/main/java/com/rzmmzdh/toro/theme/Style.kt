85f7447d2f3dc4fc4287f3baff190e71f0c44a69
rzmmzdh <rzmmzdh@gmail.com>
2022-09-25 15:11:45 +0330

    implement material theme style

---
+++ app/src/main/java/com/rzmmzdh/toro/theme/Style.kt
@@ -0,0 +1,86 @@
+package com.rzmmzdh.toro.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.sp
+
+@Immutable
+data class Style(
+    val noteCardTitle: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center
+    ),
+    val noteCardBody: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center
+    ),
+    val topBarTitle: TextStyle = TextStyle(fontFamily = vazirFontFamily,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center),
+    val searchableTopBarText: TextStyle = TextStyle(fontFamily = vazirFontFamily,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center),
+    val errorBoxTitle: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        textDirection = TextDirection.ContentOrRtl,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val errorBoxButton: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        textDirection = TextDirection.ContentOrRtl,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    val noteTitleInputValue: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center
+    ),
+    val noteTitleInputPlaceholder: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center
+    ),
+    val noteBodyInputValue: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center
+    ),
+    val noteBodyInputPlaceholder: TextStyle = TextStyle(
+        fontFamily = vazirFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        textDirection = TextDirection.ContentOrRtl,
+        textAlign = TextAlign.Center
+    ),
+)
+
+val LocalStyle = staticCompositionLocalOf { Style() }
+val MaterialTheme.style: Style
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalStyle.current
