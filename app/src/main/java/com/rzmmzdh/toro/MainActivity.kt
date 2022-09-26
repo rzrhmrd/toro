ecd5e0d4c78475df70ad95bfe44282ba2bdb1899
rzmmzdh <rzmmzdh@gmail.com>
2022-09-26 16:00:13 +0330

    hide system bars

--- app/src/main/java/com/rzmmzdh/toro/MainActivity.kt
+++ app/src/main/java/com/rzmmzdh/toro/MainActivity.kt
@@ -3,21 +3,24 @@ package com.rzmmzdh.toro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.rzmmzdh.toro.feature_note.ui.core.Screen
 import com.rzmmzdh.toro.feature_note.ui.edit_note_screen.EditNoteScreen
 import com.rzmmzdh.toro.feature_note.ui.home_screen.HomeScreen
-import com.rzmmzdh.toro.feature_note.ui.core.Screen
 import com.rzmmzdh.toro.theme.TOROTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,6 +29,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = !isSystemInDarkTheme()
+            HideSystemBar(systemUiController, useDarkIcons)
             TOROTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -53,13 +59,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun hideSystemBars() {
-        val windowInsetsController =
-            ViewCompat.getWindowInsetsController(window.decorView) ?: return
-        // Configure the behavior of the hidden system bars
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        // Hide both the status bar and the navigation bar
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    @Composable
+    fun HideSystemBar(
+        systemUiController: SystemUiController,
+        useDarkIcons: Boolean,
+    ) {
+        SideEffect {
+            systemUiController.setSystemBarsColor(color = Color.Transparent,
+                darkIcons = useDarkIcons)
+        }
     }
 }
