993c619f95fa83b2dc49d3720f247dcd07d59c2f
rzmmzdh <rzmmzdh@gmail.com>
2022-09-26 16:48:48 +0330

    implement fade in/out screen navigation animation

--- app/src/main/java/com/rzmmzdh/toro/MainActivity.kt
+++ app/src/main/java/com/rzmmzdh/toro/MainActivity.kt
@@ -3,6 +3,9 @@ package com.rzmmzdh.toro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -12,10 +15,10 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rzmmzdh.toro.feature_note.ui.core.Screen
@@ -24,6 +27,7 @@ import com.rzmmzdh.toro.feature_note.ui.home_screen.HomeScreen
 import com.rzmmzdh.toro.theme.TOROTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,19 +42,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(
+                    val navController = rememberAnimatedNavController()
+                    AnimatedNavHost(
                         navController = navController,
                         startDestination = Screen.Home.route
                     ) {
-                        composable(Screen.Home.route) {
+                        composable(route = Screen.Home.route,
+                            enterTransition = {
+                                when (initialState.destination.route) {
+                                    Screen.EditNote.route -> fadeIn()
+                                    else -> null
+                                }
+                            },
+                            exitTransition = {
+                                when (targetState.destination.route) {
+                                    Screen.EditNote.route -> fadeOut()
+                                    else -> null
+                                }
+                            }) {
                             HomeScreen(navController = navController)
                         }
-                        composable(route = Screen.EditNote.route + "?editNoteId={noteId}",
+                        composable(
+                            route = Screen.EditNote.route + "?editNoteId={noteId}",
                             arguments = listOf(navArgument("noteId") {
                                 type = NavType.IntType
                                 defaultValue = -1
-                            })) {
+                            }),
+                            enterTransition = {
+                                when (initialState.destination.route) {
+                                    Screen.Home.route -> fadeIn()
+                                    else -> null
+                                }
+                            },
+                            exitTransition = {
+                                when (targetState.destination.route) {
+                                    Screen.Home.route -> fadeOut()
+                                    else -> null
+                                }
+                            }
+                        ) {
                             EditNoteScreen(navController = navController)
                         }
                     }
