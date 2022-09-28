12bfec845ea0c192cee04941d4040f0871f751bf
rzmmzdh <rzmmzdh@gmail.com>
2022-09-28 21:08:11 +0330

    add category selected event

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/edit_note_screen/EditNoteEvent.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/edit_note_screen/EditNoteEvent.kt
@@ -5,6 +5,7 @@ sealed class EditNoteEvent {
     object ShowAlert : EditNoteEvent()
     object DeleteNote : EditNoteEvent()
     object AlertShown : EditNoteEvent()
+    data class CategorySelected(val category: NoteCategory) : EditNoteEvent()
     data class OnTitleChanged(val value: String) : EditNoteEvent()
     data class OnBodyChanged(val value: String) : EditNoteEvent()
 }
