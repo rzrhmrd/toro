928ee306cfdd998a38593d690dc581ef5cd47e9e
rzmmzdh <rzmmzdh@gmail.com>
2022-09-28 21:05:18 +0330

    add note category

---
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/edit_note_screen/NoteCategory.kt
@@ -0,0 +1,10 @@
+package com.rzmmzdh.toro.feature_note.ui.edit_note_screen
+
+enum class NoteCategory(val title: String) {
+    FREE("آزاد ✍️"),
+    NOTE("یادداشت 📝"),
+    IDEA("ایده 💡"),
+    DIARY("خاطره 🏖️"),
+    STORY("داستان ⚔️"),
+    POEM("شعر 🌊")
+}
