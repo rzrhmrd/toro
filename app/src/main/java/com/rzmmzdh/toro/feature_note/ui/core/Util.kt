f34c04a21f73759bc9427135401bd5004a48cc38
rzmmzdh <rzmmzdh@gmail.com>
2022-10-02 12:37:58 +0330

    add documentation

--- app/src/main/java/com/rzmmzdh/toro/feature_note/ui/core/Util.kt
+++ app/src/main/java/com/rzmmzdh/toro/feature_note/ui/core/Util.kt
@@ -2,6 +2,9 @@ package com.rzmmzdh.toro.feature_note.ui.core
 
 import androidx.navigation.NavController
 
+/**
+Navigate to route. By default clears the backstack.
+ */
 fun NavController.navigateTo(route: String, clearBackStack: Boolean = true) {
     navigate(route) {
         if (clearBackStack) {
