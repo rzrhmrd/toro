6beefc1f2e3840fc054f23907b102a3b78648d03
rzmmzdh <rzmmzdh@gmail.com>
2022-10-18 19:54:18 +0330

    downgrade kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ buildscript {
 plugins {
     id("com.android.application") version "7.4.0-beta02" apply false
     id("com.android.library") version "7.4.0-beta02" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
